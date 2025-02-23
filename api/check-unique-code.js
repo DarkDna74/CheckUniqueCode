a082e6188c8421ac91a1b67fd661798e44946f81
DarkDna74 <tommaso.guglielmi@gmail.com>
2025-02-23 13:51:00 +0100

    Update check-unique-code.js

--- api/check-unique-code.js
+++ api/check-unique-code.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
 
     if (error || !data) return res.status(404).json({ error: 'Codice non trovato' });
 
-    if (data.unique_code == unique_code)
+    if (String(data.unique_code) === String(unique_code))
     {
         return res.status(200).json({ valid: true });
     }
